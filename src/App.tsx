d3eeea4d9b30d75da2778def0d45dee752cce1d7
jishan <imranislamjishan80@gmail.com>
2024-07-06 18:36:27 +0600

    basic layout create

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import MainLayout from "./components/layout/MainLayout";
 
 export default function App() {
   return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto ullam doloribus in asperiores, enim quam repudiandae, voluptates similique ipsam quae accusantium dolorum ipsum, reprehenderit pariatur cum velit quos consectetur? Error?
-    </div>
-  )
+    <>
+      <MainLayout />
+    </>
+  );
 }
